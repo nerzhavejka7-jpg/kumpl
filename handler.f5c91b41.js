0985db17250d45ad61d798b9ea28f9a3ca98a52f
olegd-sketch <nerzhavejka7@gmail.com>
2025-10-10 02:24:27 +0200

    del

--- handler.f5c91b41.js
+++
@@ -1,2 +0,0 @@
-import{E as o}from"./lesson.3716610a.js";import"./axios.98430d30.js";const n=t=>document.querySelectorAll(`[data-${t}-handler]`),s=(t,e=null)=>{o.$emit(`${t}_OPEN`,e)},a=["click","mousedown","touchstart"];export{a as c,n as g,s as h};
-//# sourceMappingURL=handler.f5c91b41.js.map
--- kwiga_logo.b802ca96.js
+++
@@ -1,2 +0,0 @@
-const _="/build/assets/kwiga_logo.f13e26d4.svg";export{_};
-//# sourceMappingURL=kwiga_logo.b802ca96.js.map
